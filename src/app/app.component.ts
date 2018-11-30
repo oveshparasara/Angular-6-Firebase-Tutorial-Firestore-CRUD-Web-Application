13f224a8d6501d468e8facebfdb44c23e3af573f
Ovesh Parasara <ovesh.parasara@gmail.com>
2018-11-30 16:15:39 +0530

    remove cradancials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,12 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     const settings = {timestampsInSnapshots: true};
     const config = {
-      apiKey: "AIzaSyAmeCdnfC3R518kWcdjlKEYyId8mvjFnXI",
-      authDomain: "fir-crud-d0f70.firebaseapp.com",
-      databaseURL: "https://fir-crud-d0f70.firebaseio.com",
-      projectId: "fir-crud-d0f70",
-      storageBucket: "fir-crud-d0f70.appspot.com",
-      messagingSenderId: "922679451787"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: ""
     };
 
     firebase.initializeApp(config);
